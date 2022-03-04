0b11f082d722d61abceb16cbfa80c35a278c83d0
abdelrahman <gadoua50@gmail.com>
2022-03-05 01:04:53 +0200

    add some comments to starter-page.js

--- scripts/starter-page.js
+++ scripts/starter-page.js
@@ -1,7 +1,9 @@
+// put everthing in IIFE, to clean global scope
 (() => {
     const player1Inp = document.querySelector("#player1-inp");
     const player2Inp = document.querySelector("#player2-inp");
     
+    // define players choices to store them in local storage to use them in game.index page.
     let playerOneChoice = "";
     let playerTwoChoice = "";
     
@@ -10,6 +12,8 @@
     
     const startBtn = document.querySelector("#startBtn");
     
+    // make the player one choose X or O, then from this choice we can
+    // know the choice of player two.
     player1X.addEventListener("click", (e) => {
         player1X.disabled = true;
         player1O.disabled = false;
@@ -31,17 +35,23 @@
     startBtn.addEventListener("click", (e) => {
         const player1Name = player1Inp.value;
         const player2Name = player2Inp.value;
+
+        // if any input is empty, alert user.
         if (player1Name.length === 0 || player2Name.length === 0 || playerOneChoice.length === 0 ||
             playerTwoChoice.length === 0) {
                 
                 alert("Please complete all infomation.");
                 return;
         }
+
+
         localStorage.setItem("player1Name", player1Name);
         localStorage.setItem("player2Name", player2Name);
         localStorage.setItem("playerOneChoice", playerOneChoice);
         localStorage.setItem("playerTwoChoice", playerTwoChoice);
-        window.location.href = "./game.html";
+
+
+        window.location.href = "./game.html"; // go to game.index page
     });
 
 
